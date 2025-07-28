554d73671e3713a535dc3a29e720681c0783935d
Light1234smek <alexfotea12@gmail.com>
2025-07-28 13:55:04 +0300

    javascript.js

--- javascript.js
+++ javascript.js
@@ -1,9 +1,2 @@
-let text;
-if(Math.random() < 0.5) 
-{
-    text="<a href='https://w3schools.com'>Visit W3Schools</a>";
-}
-else {
-    text="<a href='https://wwf.org'>Visit WWF</a>";
-}
-document.getElementById("demo").innerHTML =text;
+true || alert("not printed");
+false || alert("printed");
